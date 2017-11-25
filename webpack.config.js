6bc536881ae168bb6cc32c2986811af7147c5791
nilesh <nileshgulia@gmail.com>
2017-11-25 18:10:17 +0530

    added loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,33 @@ const config={
     output:{
         filename: "bundle.js",
         path: __dirname + "/"
-    }
+    },
+    module: {
+        rules: [
+          { test: /\.css$/, use: 'css-loader' },
+          { test: /\.ts$/, use: 'ts-loader' },
+          {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {}  
+              }
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192
+                }
+              }
+            ]
+        }
+    ]
 }
+}
+
 module.exports = config;
