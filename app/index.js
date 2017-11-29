15b83d21a8a1c9109788ad462cc00f12be9a36d7
nilesh <nileshgulia@gmail.com>
2017-11-29 19:59:45 +0530

    added webpack conf

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-ReactDOM.render(<div>Hello react app</div>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,23 @@
-var path = require('path');
-const config={
-  entry: {
-    javascript: "./app/index.js",
-    html: "./build/index.html",
-  },
-  output:{
-      filename: "bundle.js",
-      path: __dirname + "./build"
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
   },
   module: {
-      rules: [
-        { test: /\.css$/, use: 'css-loader' },
-        { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-        {
-          test: [/\.(png|jpg|gif)$/,/\.html$/],
-          use: [
-            {
-              loader: 'file-loader',
-              options: {}  
-            }
-          ]
-      },
-      {
-          test: /\.(png|jpg|gif)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192
-              }
-            }
-          ]
-      },
-      {
-        test: /\.js$ /,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-  ]
-},
-devServer: {
-  contentBase: path.join(__dirname, "./build"),
-  compress: true,
-  port: 3000
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {test: /\.css$/ , loader: 'css-loader'},
+      {test: /\.css$/ , loader: 'style-loader'}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
-}
-
-module.exports = config;
