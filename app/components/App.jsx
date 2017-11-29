35ffba0ae1633ab243a3837c111d8b9196cbd3f9
nilesh <nileshgulia@gmail.com>
2017-11-29 20:39:23 +0530

    added search field

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {FormGroup, FormControl,InputGroup, Glyphicon} from 'react-bootstrap';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      query : ''
+
+    };
+    this.search=this.search.bind(this);
+   
+  }
+
+  search(){
+    console.log("clicked on search  button" , this.state.query);
+  }
+  
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World dabda </h1>
-      </div>);
+     <div>
+     <h2>Book Explorer!</h2>
+     <FormGroup>
+     <InputGroup>
+     <FormControl type="text" placeholder="Search for a book" onChange={(event) => this.setState({query : event.target.value})} />
+     <InputGroup.Addon onClick={this.search}>
+     <Glyphicon glyph="search"></Glyphicon>
+     </InputGroup.Addon>
+     </InputGroup>
+     
+     
+     </FormGroup>
+     </div>
+      );
   }
 }
